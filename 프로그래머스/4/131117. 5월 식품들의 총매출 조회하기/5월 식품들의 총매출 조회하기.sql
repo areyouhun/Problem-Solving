b35203430f909cd5a4b574fd94d03281a03ac9b7
areyouhun <97642395+areyouhun@users.noreply.github.com>
2024-04-08 16:27:29 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,8 +1,8 @@
+-- 실패
 -- select distinct fp.product_id, 
 --                 fp.product_name, 
 --                 fp.price * (
---                     select sum(amount) 
---                     from food_order 
+--                     select sum(amount) from food_order 
 --                     where product_id = fp.product_id
 --                 ) as total_sales
 -- from food_order fo
@@ -10,6 +10,7 @@
 -- where to_char(fo.produce_date, 'YYYY-MM') = '2022-05'
 -- order by total_sales desc, fp.product_id
 
+-- 성공
 select fp.product_id,
     fp.product_name,
     sum(fp.price * fo.amount) as total_sales
